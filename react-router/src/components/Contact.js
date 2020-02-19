ca45d5b6dde8aeb1017a1a74b153f3b7986af01f
Polotto <angelopolotto@gmail.com>
2020-02-19 18:09:19 -0300

    redux and react-redux configuration

--- react-router/src/components/Contact.js
+++ react-router/src/components/Contact.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-const Contact = (props) => {
+const Contact = ({cards}) => {
     //programmatic redirect
-    //console.log(props);
+    // console.log(props.cards);
     // setTimeout(() => {
     //     props.history.push('/about')
     // }, 2000);
+
+
     return ( 
         <div>
-            <div className='ui raised very padded text container segment'
-             style={{marginTop: '80px'}}>
-                <Link to='/alex' className='ui header'>Alex</Link>
-                <p>Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.</p>
-            </div>
-            <div className='ui raised very padded text container segment'
-                style={{marginTop: '80px'}}>
-                    <Link to='/willma' className='ui header'>Willma</Link>
-                    <p>Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.</p>
-            </div>
+            {
+                cards.map(card => {
+                    return (
+                        <div className='ui raised very padded text container segment'
+                            style={{marginTop: '80px'}}
+                            key={card.id}>
+                            <Link to={`/${card.title}`} className='ui header'>{card.title}</Link>
+                            <p>{card.body}</p>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
 
-export default Contact;
+const mapStateToProps = (state) => {
+    return {
+        cards: state.cards
+    }
+}
+
+export default connect(mapStateToProps)(Contact);
---
+++ react-router/src/reducers/RootReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    cards: [
+        {id: 1, title: 'Alex', body: 'Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.'},
+        {id: 2, title: 'Willma', body: 'Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.'},
+        {id: 3, title: 'John', body: 'Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.'}
+    ]
+}
+
+const RootReducer = (state = initialState, action) => {
+    return state;
+}
+
+export default RootReducer
