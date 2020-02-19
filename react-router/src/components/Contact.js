f6ff5b9424c389a4c2650b4cb35ebf0418a83381
Polotto <angelopolotto@gmail.com>
2020-02-19 10:04:51 -0300

    Route Switch tag

--- react-router/src/components/Contact.js
+++ react-router/src/components/Contact.js
@@ -11,12 +11,12 @@ const Contact = (props) => {
         <div>
             <div className='ui raised very padded text container segment'
              style={{marginTop: '80px'}}>
-                <Link to='/card/alex' className='ui header'>Alex</Link>
+                <Link to='/alex' className='ui header'>Alex</Link>
                 <p>Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.</p>
             </div>
             <div className='ui raised very padded text container segment'
                 style={{marginTop: '80px'}}>
-                    <Link to='/card/willma' className='ui header'>Willma</Link>
+                    <Link to='/willma' className='ui header'>Willma</Link>
                     <p>Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.</p>
             </div>
         </div>
