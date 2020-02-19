63e90590cb47d05490ffd9b06a245fdd77505000
Polotto <angelopolotto@gmail.com>
2020-02-19 08:48:50 -0300

    problemas em rotas duplicadas

---
+++ react-router/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return ( 
+        <div className='ui raised very padded text container segment'
+             style={{marginTop: '80px'}}>
+                <h3 className='ui header'>Contact</h3>
+                <p>Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.</p>
+        </div>
+    )
+}
+
+export default Contact;
